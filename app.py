05b20ae2dc4f3f322345d91dfe9146b644fab0dd
Trevor Liberty <tliberty@pdx.edu>
2020-06-01 22:59:20 -0700

    initial commit. leaving api key in here for now. will not be hardcoded in the future, is just used as a placeholder for playground

---
+++ app.py
@@ -0,0 +1,12 @@
+import praw
+
+reddit = praw.Reddit(client_id="zcpKwNZG8DvpMg", client_secret="urYURxN7NEao8TlE-gB_33f3ANE", user_agent="trevor")
+
+weirdo = reddit.redditor(name="spez")
+
+comments = []
+
+for comment in weirdo.comments.new(limit=None):
+    comments.append(comment.body)
+
+print(comments[0])
