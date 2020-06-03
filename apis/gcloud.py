f29231efc07b8e6cfcc2741689903b3584ccd184
Likhovodov <bitdavel@gmail.com>
2020-06-02 21:22:34 -0700

    Started on gcloud sentiment analysis

---
+++ apis/gcloud.py
@@ -0,0 +1,13 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+def extactSentiment(textData):
+        doc = types.Document(
+            content=textData,
+            type=enums.Document.Type.PLAIN_TEXT)
+        return client.analyze_sentiment(document=doc).document_sentiment.score
